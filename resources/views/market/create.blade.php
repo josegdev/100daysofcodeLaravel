bd3013063b480b7f9c06329d535a604ea3c644e6
Jose Gabriel <ortizdjose@gmail.com>
2018-08-17 13:38:10 -0400

    Primer Commit

---
+++ resources/views/market/create.blade.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="form-group">
+    <div class="container">
+        <label for="nombre"> Nombre: </label>
+        <input type="text" class="form-control" name="nombre">
+    </div>
+
+</div>
+</body>
+</html>
